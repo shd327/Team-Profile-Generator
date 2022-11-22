523ee9a9cb16cf69aaba84869c5ba2a9b9970d84
Hiro Protagonist <shd327@gmail.com>
2022-11-22 13:56:58 -0700

    required everything in index and made class file placeholders

---
+++ index.js
@@ -0,0 +1,6 @@
+const inqurier = require("inquirer")
+const fs = require("fs")
+const Employee = require("./lib/Employee")
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/Intern")
+const Manager = require("./lib/Manager")
