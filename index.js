f2caf68894030269c5f6c57065f28a2146cf9f89
Hiro Protagonist <shd327@gmail.com>
2022-11-22 14:20:16 -0700

    built manager prompt and pushing it to manager array

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
+// Requiring modules
 const inqurier = require("inquirer")
 const fs = require("fs")
 const Employee = require("./lib/Employee")
 const Engineer = require("./lib/Engineer")
 const Intern = require("./lib/Intern")
 const Manager = require("./lib/Manager")
+
+// Arrays to store managers
+var managerArray = []
+// build out manager prompt
+function promptManager() {
+    inqurier.prompt([
+        {
+            type: "input",
+            message: "what is your name",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "what is your id",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "what is your email",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "what is your office number",
+            name: "officeNumber"
+        },
+
+    ])
+        .then((data) => {
+            managerArray.push(new Manager(data.name, data.id, data.email, data.officeNumber))
+            console.log(managerArray)
+
+        })
+}
+
+
+promptManager()
