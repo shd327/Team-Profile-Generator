476372f004e0f5c4a52ff0cbc64f5da83eac573c
Hiro Protagonist <shd327@gmail.com>
2022-11-22 14:39:13 -0700

    added rest of prompts and selector prompt

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const Intern = require("./lib/Intern")
 const Manager = require("./lib/Manager")
 
 // Arrays to store managers
-var managerArray = []
+const managerArray = []
+const internArray = []
+const engineerArray = []
+
+
 // build out manager prompt
 function promptManager() {
     inqurier.prompt([
@@ -36,9 +40,109 @@ function promptManager() {
         .then((data) => {
             managerArray.push(new Manager(data.name, data.id, data.email, data.officeNumber))
             console.log(managerArray)
+            selectRole()
+
 
         })
 }
+function promptIntern() {
+    inqurier.prompt([
+        {
+            type: "input",
+            message: "what is your name",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "what is your id",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "what is your email",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "what is your school",
+            name: "school"
+        },
 
+    ])
+        .then((data) => {
+            internArray.push(new Intern(data.name, data.id, data.email, data.school))
+            selectRole()
+
+        })
+
+}
+
+function promptEngineer() {
+    inqurier.prompt([
+        {
+            type: "input",
+            message: "what is your name",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "what is your id",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "what is your email",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "what is your github",
+            name: "github"
+        },
+
+    ])
+        .then((data) => {
+            engineerArray.push(new Engineer(data.name, data.id, data.email, data.github))
+            selectRole()
+        })
+
+}
+
+function selectRole() {
+    inqurier.prompt([
+        {
+            type: "list",
+            choices: ["Intern", "Engineer", "Finishing building your team"],
+            message: "Pick a role",
+            name: "role"
+        }
+    ])
+        .then((data) => {
+            if (data.role === "Engineer") {
+                promptEngineer();
+            }
+            if (data.role === "Intern") {
+                promptIntern();
+            }
+            if (data.role === "Finishing building your team") {
+                GenerateManager(managerArray);
+
+                if (engineerArray) {
+                    GenerateEngineer(engineerArray);
+                }
+                if (internArray) {
+                    GenerateIntern(internArray);
+                }
+
+            }
+        })
+}
 
 promptManager()
+//  HTML
+// build generateManager
+// build generateIntern
+// build generateEngineer
+
+// build main html structure that we will append all the generates too using loop
+// write to file at in
